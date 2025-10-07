402cc9379db2c8df29c7f2e4f9461f634290a99d
Rikke Lund Rasmussen <soede_rikke@hotmail.com>
2025-10-07 11:36:09 +0200

    Arbejder på MoveToScouting leaf

--- Assets/Scripts/BehaviorTree/Leafs/MoveToScouting.cs
+++ Assets/Scripts/BehaviorTree/Leafs/MoveToScouting.cs
@@ -2,22 +2,59 @@ using AIGame.Core;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using MortensKombat;
 
 public class MoveToScouting : MKNode
 {
     #region Field
-    private BaseAI baseAI;
+    private BaseAI mortenAI;
+    private Vector3 startPosition;
+    private Vector3 aPosition;
+    private Vector3 bPosition;
 
     #endregion
 
-    public MoveToScouting(MKBlackboard blackboard, BaseAI baseAI) : base(blackboard)
+    #region Constructor
+    public MoveToScouting(MKBlackboard blackboard, SuperMorten superMorten) : base(blackboard)
     {
-        this.baseAI = baseAI;
-
+        this.mortenAI = superMorten;
     }
+
+    #endregion
+
+    #region Method
 
     public override NodeState Evaluate()
     {
         throw new System.NotImplementedException();
+
+        
+    }
+
+    private void Positions()
+    {
+        startPosition = mortenAI.transform.position;
+
+        var controlPoint = ControlPoint.Instance;
+
+        Vector3 cpPosition = controlPoint.transform.position;
+    }
+
+    #endregion
+
+    private bool HasReachedDestination()
+    {
+
+        if (mortenAI.GetRemainingDistance() <= ARRIVAL_THRESHOLD)
+        {
+            return true;
+        }
+        else if (!mortenAI.IsPathPending() && !mortenAI.HasPath() && Vector3.Distance(mortenAI.transform.position, currentDestination) <= ARRIVAL_THRESHOLD)
+        {
+            return true;
+        }
+
+        return false;
+
     }
 }
