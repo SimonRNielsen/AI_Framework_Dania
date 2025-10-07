195ae89940724eee51e859b1b4e541eebc369255
Rikke Lund Rasmussen <soede_rikke@hotmail.com>
2025-10-07 10:59:07 +0200

    Oprettet MoveToScouting()

---
+++ Assets/Scripts/BehaviorTree/Leafs/MoveToScouting.cs
@@ -0,0 +1,22 @@
+using AIGame.Core;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveToScouting : MKNode
+{
+    #region Field
+    private BaseAI baseAI;
+
+    #endregion
+
+    public MoveToScouting(MKBlackboard blackboard, BaseAI baseAI) : base(blackboard)
+    {
+        this.baseAI = baseAI;
+    }
+
+    public override NodeState Evaluate()
+    {
+        throw new System.NotImplementedException();
+    }
+}
