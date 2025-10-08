fb403fbcd7d413a5f4814acbb05894c125f9ce81
Bjornlund <irenebjornlund@gmail.com>
2025-10-08 21:03:53 +0200

    EnemyInRange works

--- Assets/Scripts/BehaviorTree/Leafs/EnemyInRange.cs
+++ Assets/Scripts/BehaviorTree/Leafs/EnemyInRange.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 using MortensKombat;
+using System.Linq;
 
 //set en distancetoenemy, hvis den 
 
@@ -11,6 +12,9 @@ public class EnemyInrange : MKNode
     #region Fields
     private SuperMorten supMorten;
     ControlPoint controlPoint = ControlPoint.Instance;
+    private List<Vector3> positions = new List<Vector3>();
+    private float enemyRange = 40f;
+
 
     #endregion
 
@@ -25,12 +29,26 @@ public class EnemyInrange : MKNode
     #region Methods
     public override NodeState Evaluate()
     {
-        //Vector3? closestEnemy = blackboard.GetEnemies(supMorten)
-        //    .OrderBy(x => Vector3.Distance(mortenAI.transform.position, x.position))
-        //    .FirstOrDefault()?.position;
 
+        //Finding closests enemy from blackboard
+        Vector3? closestEnemy = blackboard.GetEnemies(supMorten)
+            .OrderBy(x => Vector3.Distance(supMorten.transform.position, x.position))
+            .FirstOrDefault()?.position;
 
-        throw new System.NotImplementedException();
+        if (closestEnemy.HasValue && closestEnemy != Vector3.zero)
+        {
+            
+                if (Vector3.Distance(closestEnemy.Value, supMorten.transform.position) < enemyRange)
+                {
+                
+                    Debug.Log("IN RANGE SUCCESS");
+                    return NodeState.Success;
+
+                }
+           
+        }
+        Debug.Log("IN RANGE FAIL");
+        return NodeState.Failure;
     }
     #endregion
 }
